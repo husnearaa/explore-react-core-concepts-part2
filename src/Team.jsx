feb80d57a334d11f6f1127a97bfff6d694375254
husneara <husnearaa99@gmail.com>
2023-09-08 21:17:47 +0600

    first commit

---
+++ src/Team.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react"
+
+export default function Team(){
+    const [team, setTeam] = useState(11);
+
+    const handleAdd = () =>{
+        const newTeam = team + 1;
+        setTeam(newTeam);
+    }
+    
+    const handleRemove = () =>{
+        // const newTeam = team - 1; 
+        // setTeam(newTeam);
+        
+        // instead of previous two line, can write only this line
+        setTeam(team - 1)
+    }
+
+
+    const teamStyle = {
+        border: '2px solid purple',
+        margin: '15px',
+        padding: '15px',
+        borderRadius: '15px'
+    }
+    return(
+        <div style={teamStyle}>
+            <h3>Players:{team} </h3>
+            <button onClick={handleAdd}>Add</button>
+            <button onClick={handleRemove}>Remove</button>
+        </div>
+    )
+}
